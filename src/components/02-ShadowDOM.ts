5b9c6b9fd3224cb04fd393c5399ab7ee215bcef4
Antoine Fricker <antoine@marmelab.com>
2026-01-11 15:27:42 +0100

    feat: use shadow DOM

---
+++ src/components/02-ShadowDOM.ts
@@ -0,0 +1,19 @@
+const template = document.createElement("template");
+template.innerHTML = `<p>ShadowDOM component</p>`;
+
+export class ShadowDOM extends HTMLElement {
+  private shadow: ShadowRoot;
+
+  constructor() {
+    super();
+
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    console.log("ShadowDOM added to the DOM");
+  }
+}
+
+customElements.define("ui-shadow-dom", ShadowDOM);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 export * from "./components/01-Barebone";
+export * from "./components/02-ShadowDOM";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <div>
         <h2>Implemented in TS</h2>
         <ui-barebone></ui-barebone>
+        <ui-shadow-dom></ui-shadow-dom>
     </div>
 `;
